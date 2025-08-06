94e6714b5f0b3321ba88ffd11f18fa8ee566c86d
Arslan Creator <arslancreative25gmail.com>
2025-08-06 22:39:28 +0200

    I added list[]

--- blink.py
+++ blink.py
@@ -2,13 +2,17 @@ from machine import Pin
 from time import sleep
 led = Pin(25, Pin.OUT)
 button = Pin(14,Pin.IN,Pin.PULL_DOWN)
+list = []
 while True:
     user_input = input("Enter the value :") 
     if (user_input == "1"):
         led.value(1)
+        list.append(user_input)
         print(f"Led state{user_input} ")
     elif (user_input == "0" ):
         led.value(0)
+        list.append(user_input)
         print(f"Led state{user_input}")
     else: 
         print(f"Input invalde ! {user_input}") 
+        print(list)
